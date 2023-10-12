a39c1c1095909475071b077bf25295626fc1ac9d
Ngorhzee <ngoziamolo02@gmail.com>
2023-10-12 20:02:12 +0100

    added query

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from shared.view_tools import body_tools
 from uuid import UUID
 import typing
 import pydantic
+from rest_framework import status
 
 api = Api()
 
@@ -28,6 +29,9 @@ class ValidateMessageInput(pydantic.BaseModel):
 
 @api.endpoint("get-messages/<code>", method="GET")
 def getmessages(request: Request, code: str):
+    query_param = typing.cast(dict, request.query_params)
+    param_from = query_param.get("from")
+
     try:
         UUID(code)
     except ValueError:
@@ -37,10 +41,14 @@ def getmessages(request: Request, code: str):
     
     except ChatRoom.DoesNotExist:
         raise ResourceNotFound("Chatroom does not exist")
-    
-    messages = room.messages.all() # type: ignore
+    if param_from is None:
+       messages = room.messages.all() # type: ignore
+    else: 
+       messages = room.messages.filter(date_created__gte= param_from) # type: ignore
+
     sr = [MessageSerializer(message, request)() for message in messages]
     return Response(sr)
+
     
 
 @api.endpoint("post-messages", method="POST")
@@ -56,7 +64,7 @@ def postmessages(request: Request,):
     
     message = Messages.objects.create(chatroom=room, content=data.content, device_id=data.device_id),
 
-    return Response()
+    return Response(status=status.HTTP_204_NO_CONTENT)
         
 
 
